a8a09988c1966c27aea044ec3a9798ce7dda8f5e
MingRay <143494403+ntustRay@users.noreply.github.com>
2024-09-15 16:28:18 +0000

    Refactor page components and add new pages
    - Refactor the `HomePage` component to use arrow function syntax
    - Import `React` in the `HomePage` component
    - Update the link in the `HomePage` component to have a top margin
    - Add a new `ContactPage` component with its corresponding route
    - Add a new `InfoPage` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
+import React from 'react';
 
-function HomePage() {
+const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>Home Page</h1>
-      <Link href="/about" className="text-xl text-blue-500 inline-block m-4">
+      <Link href="/about" className='text-xl text-blue-500 inline-block mt-8'>
         about page
       </Link>
     </div>
